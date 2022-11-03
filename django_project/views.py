877d790605a6df590bf7e8e4ab076bd0f00099cf
Gabriel Londoño <105686051+gabolondon@users.noreply.github.com>
2022-11-03 00:46:31 +0000

    cambios en las views para agregar funcion de get_weather_from_location

--- django_project/views.py
+++ django_project/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import requests
-
+import os
 
 # Create your views here.
 def home(request):
@@ -16,6 +16,16 @@ def get_weather_from_ip(request):
   location = get_location_from_ip(ip_address)
   city = location.get("city")
   country_code = location.get("countryCode")
-  s = "You're in {}, {}".format(city, country_code)
+  weather_data = get_weather_from_location(city, country_code)
+  description = weather_data['weather'][0]['description']
+  temperature = weather_data['main']['temp']
+  s = "You're in {}, {}. You can expect {} with a temperature of {} degrees".format(city, country_code, description, temperature)
   data = {"weather_data": s}
   return JsonResponse(data)
+
+def get_weather_from_location(city, country_code):
+    token = os.environ.get("OPEN_WEATHER_TOKEN")
+    url = "https://api.openweathermap.org/data/2.5/weather?q={},{}&units=metric&appid= {}".format(
+        city, country_code, token)
+    response = requests.get(url)
+    return response.json()
